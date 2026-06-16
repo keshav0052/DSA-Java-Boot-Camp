a7d3bef1a6fd7ae98db5a03ccbdb0bdbbf467b4d
Keshav Sharma <keshav052sharma@gmail.com>
2026-06-16 23:29:34 +0530

    Implement Trapping Rain Water solution

---
+++ Trapping Rain water(leetcode).java
@@ -0,0 +1,23 @@
+class Solution {
+    public int trap(int[] height) {
+        int n = height.length;
+        int[] prefix = new int[n];
+        int[] suffix = new int[n];
+
+        prefix[0] = height[0];
+        suffix[n - 1] = height[n -1];
+
+        for(int i = 1; i < n; i++){
+            prefix[i] = Math.max(prefix[i - 1], height[i]);
+        }
+
+        for(int i = n - 2; i >= 0; i--) {
+            suffix[i] = Math.max(suffix[i + 1], height[i]);
+        }
+        int waterLogged = 0;
+        for(int i = 0; i < n; i++) {
+            waterLogged += Math.min(suffix[i], prefix[i]) - height[i];
+        }
+        return waterLogged;
+    }
+}
