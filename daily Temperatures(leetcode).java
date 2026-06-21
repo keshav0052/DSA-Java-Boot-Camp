49e0ae27d8c936d777806f4ffd9bbf4da00cf1ab
Keshav Sharma <keshav052sharma@gmail.com>
2026-06-21 14:33:46 +0530

    Implement dailyTemperatures function using stack

---
+++ daily Temperatures(leetcode).java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] dailyTemperatures(int[] temperatures) {
+        int n = temperatures.length;
+        Stack<Integer> st = new Stack<>();
+        int[] result = new int[n];
+
+        for(int i = n - 1; i >= 0; i--) {
+            while(!st.isEmpty() && temperatures[i] >= temperatures[st.peek()])
+                st.pop();
+
+            if(!st.isEmpty()) 
+                result[i] = st.peek() - i;
+                st.push(i);
+        }
+        return result;
+    }
+}
