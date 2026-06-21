60e5444c6c296781c14949b8f22a9ce6406436a1
Keshav Sharma <keshav052sharma@gmail.com>
2026-06-21 14:34:32 +0530

    Add pre-order traversal method for binary tree
    Implement pre-order traversal for binary tree.

---
+++ PreOrder Traversal(leetcode).java
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+
+    public static void traversal(TreeNode root, List<Integer> ans) {
+        // base 
+        if(root == null){
+            return;
+        }
+        // logic
+        ans.add(root.val);
+        traversal(root.left, ans);
+        traversal(root.right, ans);
+    }
+
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> ans = new ArrayList<>();
+        traversal(root, ans);
+        return ans;
+    }
+}
