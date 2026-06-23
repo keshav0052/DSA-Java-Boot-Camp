3f791a55dcf8bcbf1a77ab6df478bbefc3ec6457
Keshav Sharma <keshav052sharma@gmail.com>
2026-06-23 19:56:09 +0530

    Remove unnecessary whitespace in Detect Cycle in a Graph

---
+++ Detect Cycle in a Graph.java
@@ -0,0 +1,43 @@
+class Solution {
+    public boolean check(int src, int V, ArrayList<ArrayList<Integer>> adj, boolean[] visited) {
+        Queue<int[]> q = new LinkedList<>();
+        visited[src] = true;
+        q.add(new int[]{src, -1});
+        
+        while(!q.isEmpty()) {
+            int[] pair = q.poll();
+            int node = pair[0];
+            int parent = pair[1];
+        
+        for(int adjacentNode : adj.get(node)) {
+            if(!visited[adjacentNode]) {
+                visited[adjacentNode] = true;
+                q.add(new int[]{adjacentNode, node});
+            } else if (adjacentNode != parent) {
+                return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public boolean isCycle(int V, int[][] edges) {
+        // Code here
+        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+        for(int i = 0; i < V; i++) {
+            adj.add(new ArrayList<>());
+        }
+        for(int[] e : edges) {
+            adj.get(e[0]).add(e[1]);
+            adj.get(e[1]).add(e[0]);
+        }
+        boolean[] visited = new boolean[V];
+        for(int i = 0; i < V; i++) {
+            if(!visited[i]) {
+                if(check(i, V, adj, visited))
+                return true;
+            }
+        }
+        return false;
+    }
+}
